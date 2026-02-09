785e2114769e04453d837c0314769a9e8a77212d
luardino <luardino@icloud.com>
2026-02-09 03:45:38 +0100

    feat: add icons to tab navigation for Home, Schedule, Messages, and Profile screens

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,19 +1,24 @@
 import { Tabs } from "expo-router";
+import { Home2, Profile, Calendar2, Message } from "iconsax-react-native"
 
 export default function Layout() {
     return (
         <Tabs screenOptions={{ headerShown: false }}>
             <Tabs.Screen
                 name="index"
-                options={{ title: "Home" }}
+                options={{ title: "Home", tabBarIcon: ({ color, size }) => <Home2 color={color} size={size} variant="Linear"/> }}
             />
             <Tabs.Screen
-                name="schedule"
-                options={{ title: "Schedule" }}
+                name="schedule/index"
+                options={{ title: "Schedule", tabBarIcon: ({ color, size }) => <Calendar2 color={color} size={size} variant="Linear"/> }}
             />
             <Tabs.Screen
-                name="profile"
-                options={{ title: "Profile" }}
+                name="chat/index"
+                options={{ title: "Messages", tabBarIcon: ({ color, size }) => <Message color={color} size={size} variant="Linear"/> }}
+            />
+            <Tabs.Screen
+                name="profile/index"
+                options={{ title: "Profile", tabBarIcon: ({ color, size }) => <Profile color={color} size={size} variant="Linear"/> }}
             />
         </Tabs>
     );
